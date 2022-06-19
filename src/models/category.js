6a76590eb7b08e5914512f09c09006c246b7bd0a
João Tupinambá <95971013+tupis@users.noreply.github.com>
2022-06-19 00:49:50 -0300

    criação do models categoria

---
+++ src/models/category.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const category = Schema(
+    {
+        name: {type: String, required: true},
+        products: [
+            { 
+                type: Object,
+                ref: 'Product' 
+            }
+        ]
+    }
+)
+
+module.exports = mongoose.model("Category", category)
